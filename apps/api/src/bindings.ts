efbe14cfdccd61fcb337e2ebaa66bb94f7bfe946
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-12-20 21:33:55 -0600

    Initial commit

---
+++ apps/api/src/bindings.ts
@@ -0,0 +1,5 @@
+type Bindings = {
+  MAL_CLIENT_ID: string,
+}
+
+export default Bindings
---
+++ apps/api/src/index.ts
@@ -0,0 +1,35 @@
+import { Hono } from 'hono'
+import { poweredBy } from 'hono/powered-by'
+import { prettyJSON } from 'hono/pretty-json'
+
+import type Bindings from './bindings'
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+// TODO: CORS and CSRF
+app.use('*', poweredBy());
+app.use('*', prettyJSON());
+
+app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404));
+
+app.get('/', (c) => {
+  return c.json({ message: 'Yo!' })
+})
+
+app.get('/health', async (c) => {
+  const response = await fetch('https://api.myanimelist.net/v2/anime/21273',
+    { headers: { 'X-MAL-CLIENT-ID': c.env.MAL_CLIENT_ID } }
+  );
+
+  console.log("Actual MAL response: ", await response.text());
+
+  if (response.ok) {
+    return c.json({ ok: true, services: ['mal'] });
+  } else {
+    // I know that I should probably return a 503 (or whatever MAL sends us),
+    // but I don't want to signal to the frontend that the service is unavailable.
+    return c.json({ ok: false, services: [], message: `MAL is unavailable: ${response.status}` });
+  }
+})
+
+export default app
